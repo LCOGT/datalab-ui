0c8241da93349c685848f2c1a72f2dc872a698c6
Lloyd Dakin <taidakin99lol@gmail.com>
2025-02-24 10:54:58 -0800

    analysis store

---
+++ src/stores/analysis.js
@@ -0,0 +1,72 @@
+import { defineStore } from 'pinia'
+import { useConfigurationStore } from '@/stores/configuration' 
+import { fetchApiCall } from '@/utils/api.js'
+
+/**
+ * This store manages the images and analysis results displayed on the analysis page.
+ */
+export const useAnalysisStore = defineStore('analysis', {
+  state: () => ({
+    image: null, // image archive object
+    rawData: null, // raw data from the analysis/raw_data endpoint
+    zmin: null, // minimum z value of the raw data
+    zmax: null, // maximum z value of the raw data
+    imageUrl: '', // URL of the image to display
+    imageWidth: null, // width of the image in pixels
+    imageHeight: null, // height of the image in pixels
+  }),
+  getters: {
+    imageScaleReady: (state) => state.imageWidth && state.imageHeight && state.rawData && state.zmin && state.zmax,
+  },
+  actions: {
+    async loadScaleData() {
+      await this.loadImageDimensions()
+      await this.loadRawData()
+    },
+    // Load image dimensions
+    loadImageDimensions() {
+      if (!this.imageUrl) {
+        console.error('No image URL provided')
+        return
+      }
+
+      const url = this.imageUrl || this.image.largeCachedUrl
+      const img = new Image()
+      img.src = url
+      return new Promise((resolve) => {
+        img.onload = () => {
+          this.imageWidth = img.width
+          this.imageHeight = img.height
+          resolve()
+        }
+      })
+    },
+    async loadRawData() {
+      const configStore = useConfigurationStore()
+      const datalabApiBaseUrl = configStore.datalabApiBaseUrl
+      const url = datalabApiBaseUrl + 'analysis/raw-data/'
+
+      if(!this.image){
+        console.error('No image object provided')
+        return
+      }
+
+      const requestBody = {
+        'basename': this.image.basename,
+        'source': this.image.source,
+        'max_size': Math.max(this.imageWidth, this.imageHeight),
+      }
+
+      await fetchApiCall({url: url, method: 'POST', body: requestBody,
+        successCallback: (response) => {
+          this.rawData = response
+          this.zmin = response.zmin
+          this.zmax = response.zmax
+        },
+        failCallback: (error) => {
+          console.error('Could not load raw_data', error)
+        }
+      })
+    },
+  },
+})
