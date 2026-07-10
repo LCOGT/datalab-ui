a16e57d473bdf0221066ee7b8595a9c5ee5c4998
Carolina Capetillo <carolinacapetilloc@gmail.com>
2026-07-10 14:33:32 -0700

    updates download logic to include title and subtitles and colors

--- src/utils/downloadChart.js
+++ src/utils/downloadChart.js
@@ -1,17 +1,29 @@
-export async function downloadChartAsPNG(chart, filename, titleText) {
-  // Deep clone original options and datasets
-  const originalOptions = JSON.parse(JSON.stringify(chart.options))
+export async function downloadChartAsPNG(chart, filename, titleText, options = {}) {
+  const { preserveDatasetColors = false } = options
+  const originalTitle = chart.options.plugins?.title
+  const originalTooltip = chart.options.plugins?.tooltip
+  const originalLegendLabelColor = chart.options.plugins?.legend?.labels?.color
+  const originalScaleStyles = {}
   const originalDatasets = chart.data.datasets.map(ds => ({ ...ds }))
-  chart.data.datasets.forEach(ds => {
-    ds.borderColor = '#000'
-    ds.backgroundColor = '#000'
-  })
+
+  if (!preserveDatasetColors) {
+    chart.data.datasets.forEach(ds => {
+      ds.borderColor = '#000'
+      ds.backgroundColor = '#000'
+    })
+  }
 
   // Set all option colors to black, background to transparent
   if (chart.options.plugins?.legend?.labels) chart.options.plugins.legend.labels.color = '#000'
   if (chart.options.plugins) chart.options.plugins.tooltip = { enabled: false }
   if (chart.options.scales) {
-    Object.values(chart.options.scales).forEach(scale => {
+    Object.entries(chart.options.scales).forEach(([scaleId, scale]) => {
+      originalScaleStyles[scaleId] = {
+        ticksColor: scale.ticks?.color,
+        titleColor: scale.title?.color,
+        gridColor: scale.grid?.color,
+        borderColor: scale.border?.color,
+      }
       if (scale.ticks) scale.ticks.color = '#000'
       if (scale.title) scale.title.color = '#000'
       if (scale.grid) scale.grid.color = '#000'
@@ -39,7 +51,23 @@ export async function downloadChartAsPNG(chart, filename, titleText) {
   a.click()
 
   // Restore original colors
-  chart.options = originalOptions
+  if (chart.options.plugins) {
+    chart.options.plugins.title = originalTitle
+    chart.options.plugins.tooltip = originalTooltip
+    if (chart.options.plugins.legend?.labels) {
+      chart.options.plugins.legend.labels.color = originalLegendLabelColor
+    }
+  }
+  if (chart.options.scales) {
+    Object.entries(chart.options.scales).forEach(([scaleId, scale]) => {
+      const originalStyle = originalScaleStyles[scaleId]
+      if (!originalStyle) return
+      if (scale.ticks) scale.ticks.color = originalStyle.ticksColor
+      if (scale.title) scale.title.color = originalStyle.titleColor
+      if (scale.grid) scale.grid.color = originalStyle.gridColor
+      if (scale.border) scale.border.color = originalStyle.borderColor
+    })
+  }
   chart.data.datasets = originalDatasets
   chart.update('none')
 }
